36b0fe578cf081bd127a37a09a1b627e73bf6728
lukesummers-20 <lsummers@g.hmc.edu>
2025-04-27 16:39:06 -0700

    fixed serial connec variables

--- app/backend/main.py
+++ app/backend/main.py
@@ -18,10 +18,10 @@ from flask import Flask, request, jsonify, Response
 app = Flask(__name__)
 
 # 1. --- USER SETTINGS -------------------------------------------------
-# port       = "/dev/tty.usbmodemF412FA75DE8C2"        # Windows example – replace with /dev/ttyACM0, /dev/ttyUSB0, etc.
-# baud_rate  = 9600        # Must match Serial.begin() in your sketch
-# log_to_csv = False         # True => also append to a CSV file
-# csv_path   = "arduino_log.csv"
+port       = "/dev/tty.usbmodemF412FA75DE8C2"        # Windows example – replace with /dev/ttyACM0, /dev/ttyUSB0, etc.
+baud_rate  = 9600        # Must match Serial.begin() in your sketch
+log_to_csv = False         # True => also append to a CSV file
+csv_path   = "arduino_log.csv"
 # ---------------------------------------------------------------------
 
 def open_serial(wait_for_arduino_reset: bool = True) -> serial.Serial:
